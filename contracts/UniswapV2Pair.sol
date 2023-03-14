b5b50ad37a70ff6e1c7a245b09ed8151be9849f3
vijayrajpanchall <vijayrajpanchal4@gmail.com>
2023-03-14 16:34:31 +0530

    Added uniswap contracts

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -11,7 +11,7 @@ import './interfaces/IUniswapV2Callee.sol';
 
 
 contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
-    using SafeMath5 for uint;
+    using SafeMath5  for uint;
     using UQ112x112 for uint224;
 
     uint public constant MINIMUM_LIQUIDITY = 10**3;
@@ -20,8 +20,6 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     address public factory;
     address public token0;
     address public token1;
-   // address public wallet = 0x4D9ec0254e66EDC0067Df74b28Fff10651CA236b;
-
 
     uint112 private reserve0;           // uses single storage slot, accessible via getReserves
     uint112 private reserve1;           // uses single storage slot, accessible via getReserves
@@ -171,25 +169,8 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         address _token0 = token0;
         address _token1 = token1;
         require(to != _token0 && to != _token1, 'UniswapV2: INVALID_TO');
-        //modified code
-        if (amount0Out > 0){ 
-             
-             uint treasuryFee = amount0Out.mul(2).div(100);    
-            uint amount0OutAfter = amount0Out.sub(treasuryFee);
-            _safeTransfer(_token0, IUniswapV2Factory(factory).treasuryad(), treasuryFee);
-            _safeTransfer(_token0, to, amount0OutAfter);
-            
-
-         } 
-        if (amount1Out > 0){
-            
-               uint treasuryFee = amount1Out.mul(2).div(100);    
-            uint amount1OutAfter = amount1Out.sub(treasuryFee);
-              _safeTransfer(_token1, IUniswapV2Factory(factory).treasuryad(), treasuryFee);
-            _safeTransfer(_token1, to, amount1OutAfter);
-          
-             }
-
+        if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
+        if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
         if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
@@ -197,7 +178,12 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
-       
+        { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
+        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
+        uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');
+        }
+
         _update(balance0, balance1, _reserve0, _reserve1);
         emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
     }
